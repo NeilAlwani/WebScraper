f3a0b5da60a6f4116af7fd67aa91c1da69a417b2
nalwani <N.M.Alwani@student.tudelft.nl>
2020-06-08 12:06:22 +0200

    Base crawler created

--- myScraper/myScraper/spiders/scrapeCode.py
+++ myScraper/myScraper/spiders/scrapeCode.py
@@ -2,21 +2,34 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+   #Name of the crawler
+    name = "crawler"
 
+    # Method that starts requests
     def start_requests(self):
         urls = [
             'https://www.bol.com/nl/s/?searchtext=wireless+earphones&searchContext=media_all&appliedSearchContextId=&suggestFragment=&adjustedSection=&originalSection=main&originalSearchContext=media_all&section=main&N=0&defaultSearchContext=media_all&suggestionType=search_history'
         ]
+
+        # For each url open a request to that page
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    # Method that parses the response object using css operators
     def parse(self, response):
-       for title in response.css('.product-title::text'):
-        yield{
-            "title": title.get()
-        }
+        post = response.css('div.results-area')
+        # Small check to add the correct price tags to the titles
+        for x in range(0,13):
+            if x % 2 == 0:
+                y = x
+            else:
+                y = x + 1
+            yield{
+                'title': post.css('div.product-title--inline a::text')[x].get(),
+                'price': post.css('.promo-price::text')[y].get()
+            }
 
-        #for post in response.css('div.results-area).
-        #   title = post.css('div.product-title--inline a::text')[0].get()
-        #   price = post.css('meta')[0].get
+
+
+            
+# Run with: scrapy crawl crawler -o qoutes.json
